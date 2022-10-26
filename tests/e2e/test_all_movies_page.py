8f89fe000910443cff35b5694d1fd7036dfb2b54
Emre Myuftyuoglu <emre.myuftyuoglu@gmail.com>
2022-10-25 23:36:32 -0400

    Added functionality to view saved movies, directors and ratings

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,35 @@
 # TODO: Feature 1
+from urllib import response
+from flask.testing import FlaskClient
+
+
+def test_home_page(test_app: FlaskClient):
+
+    test_app.post("/movies", data={
+        "movie_title": "Avatar",
+        "movie_director": "James Cameron",
+        "movie_rating": "5"
+    })
+    
+    test_app.post("/movies", data={
+        "movie_title": "James Bond",
+        "movie_director": "Ian Fleming",
+        "movie_rating": "2"
+    })
+        
+    test_app.post("/movies", data={
+        "movie_title": "Barbarian",
+        "movie_director": "Zach Cregger",
+        "movie_rating": "4"
+    })
+    
+    
+    response = test_app.get("/movies")
+    response_data = response.data()
+    
+    
+    assert b'<tr>Table</tr>' in response_data
+    
+   
+
+    
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,18 @@
 # TODO: Feature 1
+
+from src.repositories import movie_repository
+
+
+def test_movie_model():
+    
+    allMovie = movie_repository.get_movie_repository()
+    
+    assert len(allMovie.get_all_movies()) == 0
+    
+    allMovie.create_movie("Avatar", "Director1", 4)
+    allMovie.create_movie("Fast and Furious", "Director2", 1)
+    allMovie.create_movie("Smile", "Director3", 2)
+    
+    assert len(allMovie.get_all_movies()) != 0
+    
+    
